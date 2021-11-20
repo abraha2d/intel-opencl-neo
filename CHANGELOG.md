26a93e3e6d7e4e2bd77637edd271d9e8cb4ddbc4
Kevin Abraham <abraha2d@users.noreply.github.com>
2021-11-20 14:17:19 -0500

    Add --auto-deconfigure to dpkg arguments, and scrape new release page format properly

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Changelog
+
+
+## v0.3.0 (2021-11-20)
+
+`intel-opencl` has been replaced by `intel-opencl-icd` since ICR 21.42.21270.
+After updating past this version of ICR, you may need to run `sudo apt -f install` to fix conflicts.
+
+Also, GitHub's release page format has changed slightly.
+
+### Changes
+- Updated installer to allow for de-configuration of `intel-opencl`.
+- Updated scraper to parse GitHub's new release page format.
+- Added `bootstrap.sh` to set up a virtual environment with the necessary dependencies.
+
+
+## v0.2.0 (2020-12-17)
+
+Intel Compute Runtime and Intel Graphics Compiler are released in separate repos since ICR 20.37.17906 / IGC 1.0.4944.
+
+### Changes
+- Updated installer to download assets from both repositories.
+- Added dependencies to the README.
+
+
+## v0.1.0 (2019-12-15)
+
+Initial release
--- install.py
+++ install.py
@@ -5,11 +5,9 @@ An installer for the Intel(R) Graphics Compute Runtime for OpenCL(TM).
 Installs the requested version from the configured repository.
 """
 
-VERSION = "0.2.1"
-
+__version__ = "0.3.0"
 
 import cssselect
-import curses
 import lxml.html
 import os
 import requests
@@ -18,8 +16,7 @@ import sys
 import tempfile
 import tqdm
 
-
-#---------------
+# ---------------
 # Configuration
 #
 
@@ -30,234 +27,244 @@ CHUNK_SIZE = 8192
 
 DEBUG = True
 
-
-#------------------------
+# ------------------------
 # Advanced Configuration
 #
 
 RELEASE_URL = "https://github.com/{}/releases/{}"
 
-ASSET_SELECTOR = ".Box > div > .d-flex > a"
+ASSET_SELECTOR = "li.Box-row > a"
 
 
-#-----------
+# -----------
 # Utilities
 #
 
 class C:
-  BLK = 0
-  RED = 1
-  GRN = 2
-  YEL = 3
-  BLU = 4
-  MAG = 5
-  CYA = 6
-  WHT = 7
+    BLK = 0
+    RED = 1
+    GRN = 2
+    YEL = 3
+    BLU = 4
+    MAG = 5
+    CYA = 6
+    WHT = 7
+
 
 def c(fg=None, bg=None, fg_bright=False, bg_bright=False):
-  l = []
-  if fg is not None: l.append(str(fg + (90 if fg_bright else 30)))
-  if bg is not None: l.append(str(bg + (100 if bg_bright else 40)))
-  return f"\x1B[{';'.join(l)}m"
+    parts = []
+    if fg is not None:
+        parts.append(str(fg + (90 if fg_bright else 30)))
+    if bg is not None:
+        parts.append(str(bg + (100 if bg_bright else 40)))
+    return f"\x1B[{';'.join(parts)}m"
 
-OK =    (' OK ', c(C.GRN), True)
+
+OK = (' OK ', c(C.GRN), True)
 EMPTY = ('    ', c(C.WHT), False)
-INFO =  ('INFO', c(C.CYA), False)
-DBUG =  ('DBUG', c(C.MAG), False)
-WARN =  ('WARN', c(C.YEL), False)
-FAIL =  ('FAIL', c(C.RED), True)
-
-def print_(type, *args, **kwargs):
-  if type == DBUG and not DEBUG:
-    return
-  prefix = f"\x1B[G{type[1]}[{type[0]}]{c()}"
-  if kwargs.pop('replace', type[2]):
-    prefix = "\x1B[A" + prefix
-    if args:
-      args = args + ("\x1B[K",)
-  kwargs.pop('sep', None)
-  print(prefix, *args, **kwargs, sep=' ')
+INFO = ('INFO', c(C.CYA), False)
+DBUG = ('DBUG', c(C.MAG), False)
+WARN = ('WARN', c(C.YEL), False)
+FAIL = ('FAIL', c(C.RED), True)
 
 
-#-----------
+def print_(p_type, *args, **kwargs):
+    if p_type == DBUG and not DEBUG:
+        return
+    prefix = f"\x1B[G{p_type[1]}[{p_type[0]}]{c()}"
+    if kwargs.pop('replace', p_type[2]):
+        prefix = "\x1B[A" + prefix
+        if args:
+            args = args + ("\x1B[K",)
+    kwargs.pop('sep', None)
+    print(prefix, *args, **kwargs, sep=' ')
+
+
+def handle_connection_error(e):
+    print_(FAIL)
+    try:
+        e = e.args[0]
+        e = e.reason
+        e = e.args[0]
+        e = e.split(": ")[-1]
+    finally:
+        print_(INFO, f"Encountered {e}.")
+    exit(1)
+
+
+def handle_encoding_error(e):
+    print_(FAIL)
+    try:
+        e = e.args[-1]
+        e = e.args[-1]
+        e = str(e)
+    finally:
+        print_(INFO, f"Encountered {e}.")
+    exit(1)
+
+
+# -----------
 # Functions
 #
 
 def get_release_page(repo, version="latest"):
-  """
+    """
   Retrieves the HTML page corresponding to the requested version.
   """
-  try:
-    if version == "latest":
-      print_(EMPTY, "Getting latest release details...")
-      r = requests.get(RELEASE_URL.format(repo, version))
-    else:
-      print_(EMPTY, f"Getting details for release {version}...")
-      r = requests.get(RELEASE_URL.format(repo, f"tag/{version}"))
-  except requests.exceptions.RequestException as e:
-    print_(FAIL)
     try:
-      e = e.args[0]
-      e = e.reason
-      e = e.args[0]
-      e = e.split(": ")[-1]
-    finally:
-      print_(INFO, f"Encountered {e}.")
-    exit(1)
+        if version == "latest":
+            print_(EMPTY, "Getting latest release details...")
+            r = requests.get(RELEASE_URL.format(repo, version))
+        else:
+            print_(EMPTY, f"Getting details for release {version}...")
+            r = requests.get(RELEASE_URL.format(repo, f"tag/{version}"))
+    except requests.exceptions.RequestException as e:
+        handle_connection_error(e)
+        return
 
-  if r:
-    print_(OK)
-    if version == "latest":
-      latest_version = r.url.split('/')[-1]
-      print_(INFO, f"Latest release: {latest_version}")
-  else:
-    print_(FAIL)
-    if r.status_code == 404:
-      print_(INFO, "Release not found.")
+    if r:
+        print_(OK)
+        if version == "latest":
+            latest_version = r.url.split('/')[-1]
+            print_(INFO, f"Latest release: {latest_version}")
     else:
-      print_(INFO, f"Status code: {r.status_code}")
-    exit(1)
+        print_(FAIL)
+        if r.status_code == 404:
+            print_(INFO, "Release not found.")
+        else:
+            print_(INFO, f"Status code: {r.status_code}")
+        exit(1)
 
-  return r.text
+    return r.text
 
 
-def download_asset(asset, dir):
-  """
+def download_asset(asset, directory):
+    """
   Downloads the asset from the specified URL into the specified directory.
   """
-  asset_basename = os.path.basename(asset)
-  print_(EMPTY, f"Downloading asset: {asset_basename}")
-  try:
-    r = requests.get(asset, stream=True)
-    if not r:
-      print_(FAIL)
-      if r.status_code == 404:
-        print_(INFO, "Asset not found.")
-      else:
-        print_(INFO, f"Status code: {r.status_code}")
-      exit(1)
-    with open(os.path.join(dir, asset_basename), 'wb') as f:
-      with tqdm.tqdm(
-        total=int(r.headers['Content-Length']),
-        leave=False, miniters=1,
-        unit='B', unit_scale=True
-      ) as pbar:
-        try:
-          for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
-            if chunk:
-              f.write(chunk)
-              f.flush()
-              pbar.update(len(chunk))
-        except KeyboardInterrupt as e:
-          print("\x1B[A\x1B[2K", end='')
-          raise e
-  except requests.exceptions.ChunkedEncodingError as e:
-    print_(FAIL)
+    asset_basename = os.path.basename(asset)
+    print_(EMPTY, f"Downloading asset: {asset_basename}")
     try:
-      e = e.args[-1]
-      e = e.args[-1]
-      e = str(e)
-    finally:
-      print_(INFO, f"Encountered {e}.")
-    exit(1)
-  except requests.exceptions.ConnectionError as e:
-    print_(FAIL)
-    try:
-      e = e.args[0]
-      e = e.reason
-      e = e.args[0]
-      e = e.split(": ")[-1]
-    finally:
-      print_(INFO, f"Encountered {e}.")
-    exit(1)
+        r = requests.get(asset, stream=True)
+        if not r:
+            print_(FAIL)
+            if r.status_code == 404:
+                print_(INFO, "Asset not found.")
+            else:
+                print_(INFO, f"Status code: {r.status_code}")
+            exit(1)
+        with open(os.path.join(directory, asset_basename), 'wb') as f:
+            with tqdm.tqdm(
+                    total=int(r.headers['Content-Length']),
+                    leave=False, miniters=1,
+                    unit='B', unit_scale=True
+            ) as pbar:
+                try:
+                    for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+                        if chunk:
+                            f.write(chunk)
+                            f.flush()
+                            pbar.update(len(chunk))
+                except KeyboardInterrupt as e:
+                    print("\x1B[A\x1B[2K", end='')
+                    raise e
+    except requests.exceptions.ChunkedEncodingError as e:
+        handle_encoding_error(e)
+    except requests.exceptions.ConnectionError as e:
+        handle_connection_error(e)
+        return
 
-  print_(OK)
+    print_(OK)
 
 
-def download_assets(page, dir=None):
-  """
+def download_assets(page, directory=None):
+    """
   Downloads assets from the given page into a temporary directory.
   """
-  print_(EMPTY, "Compiling list of assets to download...")
-  try:
-    expression = cssselect.HTMLTranslator().css_to_xpath(ASSET_SELECTOR)
-  except cssselect.SelectorError:
-    print_(FAIL)
-    print_(INFO, "Invalid ASSET_SELECTOR configured.")
-    exit(1)
-  asset_list = [
-    "https://github.com" + e.get('href')
-    for e in lxml.html.fromstring(page).xpath(expression)]
-  print_(OK)
+    print_(EMPTY, "Compiling list of assets to download...")
+    try:
+        expression = cssselect.HTMLTranslator().css_to_xpath(ASSET_SELECTOR)
+    except cssselect.SelectorError:
+        print_(FAIL)
+        print_(INFO, "Invalid ASSET_SELECTOR configured.")
+        exit(1)
+        return
+    asset_list = [
+        "https://github.com" + e.get('href')
+        for e in lxml.html.fromstring(page).xpath(expression)
+        if "Source code" not in e[0].text
+    ]
+    print_(OK)
 
-  tmp_dir = dir or tempfile.mkdtemp(prefix='intel-opencl-neo-')
-  if dir is None:
-    print_(DBUG, f"Temporary directory: {tmp_dir}")
+    tmp_dir = directory or tempfile.mkdtemp(prefix='intel-opencl-neo-')
+    if directory is None:
+        print_(DBUG, f"Temporary directory: {tmp_dir}")
 
-  for asset in asset_list:
-    download_asset(asset, tmp_dir)
+    for asset in asset_list:
+        download_asset(asset, tmp_dir)
 
-  return tmp_dir
-
-
-def run_command(cmd, dir):
-  process = subprocess.Popen(
-    cmd,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    shell=True,
-    cwd=dir,
-    universal_newlines=True,
-    bufsize=1,
-  )
-
-  i = 0
-
-  try:
-    for line in iter(process.stdout.readline, ''):
-      i += 1
-      print(line, end='')
-      sys.stdout.flush()
-    process.wait()
-    failure = process.returncode
-  except KeyboardInterrupt as e:
-    print("\x1B[A"*i, end='')
-    print_(FAIL)
-    print("\x1B[B"*i, end='')
-    print_(INFO, "Interrupted.")
-    exit(2)
-
-  print("\x1B[A"*i, end='')
-  print_(FAIL if failure else OK)
-  print("\x1B[B"*i, end='')
-  if failure:
-    exit(1)
+    return tmp_dir
 
 
-def verify_assets(dir):
-  """
+def run_command(cmd, directory):
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True,
+        cwd=directory,
+        universal_newlines=True,
+        bufsize=1,
+    )
+
+    i = 0
+
+    try:
+        for line in iter(process.stdout.readline, ''):
+            i += 1
+            print(line, end='')
+            sys.stdout.flush()
+        process.wait()
+        failure = process.returncode
+    except KeyboardInterrupt:
+        print("\x1B[A" * i, end='')
+        print_(FAIL)
+        print("\x1B[B" * i, end='')
+        print_(INFO, "Interrupted.")
+        exit(2)
+        return
+
+    print("\x1B[A" * i, end='')
+    print_(FAIL if failure else OK)
+    print("\x1B[B" * i, end='')
+    if failure:
+        exit(1)
+
+
+def verify_assets(directory):
+    """
   Verifies integrity of assets in the specified directory.
   """
-  print_(EMPTY, "Verifying assets...")
-  run_command("sha256sum -c *.sum", dir)
+    print_(EMPTY, "Verifying assets...")
+    run_command("sha256sum -c *.sum", directory)
 
 
-def install_assets(dir):
-  """
+def install_assets(directory):
+    """
   Installs assets from the specified directory.
   """
-  print_(EMPTY, "Installing assets...")
-  run_command("sudo dpkg -i *.deb", dir)
+    print_(EMPTY, "Installing assets...")
+    run_command("sudo dpkg -iRB .", directory)
 
 
 def print_usage():
-  """
+    """
   Prints the usage of the program.
   """
 
-  basename = os.path.basename(sys.argv[0])
+    basename = os.path.basename(sys.argv[0])
 
-  print(f"""{__doc__}
+    print(f"""{__doc__}
 Configured repositories:
 - {GH_REPO_RUNTIME}
 - {GH_REPO_COMPILER}
@@ -276,51 +283,51 @@ Options:
 """)
 
 
-#------------
+# ------------
 # Main Logic
 #
 
 def main():
-  rPage = get_release_page(GH_REPO_RUNTIME, sys.argv[1] if len(sys.argv) > 2 else "latest")
-  dir = download_assets(rPage)
+    runtime_page = get_release_page(GH_REPO_RUNTIME, sys.argv[1] if len(sys.argv) > 2 else "latest")
+    directory = download_assets(runtime_page)
 
-  cPage = get_release_page(GH_REPO_COMPILER, sys.argv[2] if len(sys.argv) > 2 else "latest")
-  download_assets(cPage, dir)
+    compiler_page = get_release_page(GH_REPO_COMPILER, sys.argv[2] if len(sys.argv) > 2 else "latest")
+    download_assets(compiler_page, directory)
 
-  verify_assets(dir)
-  install_assets(dir)
+    verify_assets(directory)
+    install_assets(directory)
 
-  print_(OK, "All done.", replace=False)
+    print_(OK, "All done.", replace=False)
 
 
 if __name__ == "__main__":
-  if "-h" in sys.argv or "--help" in sys.argv:
-    print_usage()
-    exit(0)
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print_usage()
+        exit(0)
 
-  if "-v" in sys.argv or "--version" in sys.argv:
-    print(f"{os.path.basename(sys.argv[0])} v{VERSION}")
-    exit(0)
+    if "-v" in sys.argv or "--version" in sys.argv:
+        print(f"{os.path.basename(sys.argv[0])} v{__version__}")
+        exit(0)
 
-  for arg in sys.argv:
-    if arg[0] == "-":
-      print_(FAIL, f"Unknown option \"{arg}\"", replace=False)
-      print_usage()
-      exit(1)
+    for arg in sys.argv:
+        if arg[0] == "-":
+            print_(FAIL, f"Unknown option \"{arg}\"", replace=False)
+            print_usage()
+            exit(1)
 
-  if len(sys.argv) > 1 and len(sys.argv) < 3:
-    print_(FAIL, "Not enough arguments.", replace=False)
-    print_usage()
-    exit(1)
+    if 1 < len(sys.argv) < 3:
+        print_(FAIL, "Not enough arguments.", replace=False)
+        print_usage()
+        exit(1)
 
-  if len(sys.argv) > 3:
-    print_(FAIL, "Too many arguments.", replace=False)
-    print_usage()
-    exit(1)
+    if len(sys.argv) > 3:
+        print_(FAIL, "Too many arguments.", replace=False)
+        print_usage()
+        exit(1)
 
-  try:
-    main()
-  except KeyboardInterrupt:
-    print_(FAIL)
-    print_(INFO, "Interrupted.")
-    exit(2)
+    try:
+        main()
+    except KeyboardInterrupt:
+        print_(FAIL)
+        print_(INFO, "Interrupted.")
+        exit(2)
